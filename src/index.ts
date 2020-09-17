f5ae94e969a35cd5533c8c0812234f9714be55d1
Danny Springett-McHugh <danny.springett-mchugh@netcall.com>
2020-09-17 12:42:02 +0100

    Added the additional exports for the new helpers, interfaces, modules and enums.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import './module/internal';
 
 import { ObjectID as ModelRecordID } from 'mongodb';
 
+import { Packet } from './helper/packet';
+import { WSHelper } from './helper/ws-helper';
+
 import { Controller } from './decorator/controller';
 import { Route } from './decorator/route';
 import { Model } from './decorator/model';
@@ -12,19 +15,31 @@ import { Authenticated } from './decorator/authenticated';
 import { AbstractModel } from './abstract/model';
 import { AbstractController } from './abstract/controller';
 
+import { IAny } from './interface/any';
 import { IContext } from './interface/context';
 import { IModelDefinition } from './interface/model-definition';
 import { IModelField } from './interface/model-field';
 import { IModelQuery } from './interface/model-query';
 import { IOptions } from './interface/options';
+import { IPacket } from './interface/packet';
 import { IRecord } from './interface/record';
 import { IReturn } from './interface/return';
 import { IRoute } from './interface/route';
 import { IValidateResponse } from './interface/validate-response';
 
+import { Errors } from './enum/errors';
+
 import { Server } from './module/server';
+import { WSServer } from './module/ws-server';
 
 export {
+
+	// External
+	ModelRecordID,
+
+	// Helpers
+	Packet,
+	WSHelper,
 
 	// Decorators
 	Controller,
@@ -38,17 +53,22 @@ export {
 	AbstractController,
 
 	// Interfaces
+	IAny,
 	IContext,
 	IModelDefinition,
 	IModelField,
 	IModelQuery,
 	IOptions,
+	IPacket,
 	IRecord,
 	IReturn,
 	IRoute,
 	IValidateResponse,
 
+	// Enums
+	Errors,
+
 	// Modules
 	Server,
-	ModelRecordID,
+	WSServer,
 }
